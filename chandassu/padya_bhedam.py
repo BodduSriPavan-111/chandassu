b3684683b340ec0419e1e22d15de718897f3fd13
BodduSriPavan-111 <boddusripavan111@gmail.com>
2025-08-07 22:13:11 +0530

    feat: weightage-factor-implementation-started-chandassu-score-vruttamu

--- chandassu/padya_bhedam.py
+++ chandassu/padya_bhedam.py
@@ -8,9 +8,109 @@ License: MIT
 from .check_lakshanam import *
 from .padyam_config import *
 
-def check_vruttam( lg_data, type, weightage_factor= 1, verbose= True ):
+def check_teytageethi( lg_data, type= "teytageethi", weightage_factor= 1, verbose= True ):
 
     try:
+        config= getattr(VupaJaathi, type, False)
+
+        padamwise_ganam_data= []
+
+        gana_kramam_score= 0
+        end= 0
+        paadam_count= 0
+
+        while end< len(lg_data):
+
+            ganam_data= []
+
+            for j in range( len(config["gana_kramam"]) ):
+
+                for i in config["gana_kramam"][j]:
+
+                    # Take legth of corresponding ganam
+                    ganam= tuple([k[1] for k in lg_data[end: end+len(ganamulu[i]) ]])
+
+                    if verbose:
+                        print("Ganam : ", ganam)
+
+                    try:
+                        if r_ganamulu[ ganam ] == i:
+                            
+                            ganam_data.append( [lg_data[end: end+len(ganamulu[i])], r_ganamulu[ganam]] )
+
+                            gana_kramam_score+= 1
+                            
+                            if verbose:
+                                print( [lg_data[end: end+len(ganamulu[i])], r_ganamulu[ganam]] )
+
+                            break
+                
+                    except Exception as e:
+                        print(e)
+                        pass
+
+                # Increment with the last ganam length (maximum)
+                end+= len(ganamulu[i])
+            
+            paadam_count+= 1
+            padamwise_ganam_data.append( ganam_data )
+
+            if verbose:
+                print( ganam_data )
+
+        match_yati= []
+        for line in padamwise_ganam_data:
+
+            if verbose:
+                print(line)
+            
+            yati_value= check_yati(     
+                                        yati_sthanam= True,
+                                        paadam= line, 
+                                        first_letter= line[0][0][0][0], 
+                                        yati_sthanam_letter= line[config["yati_sthanam"][0]-1][0][0][0], 
+                                        verbose= True 
+                                    )
+            
+            match_yati.append(yati_value)
+
+            if verbose:
+                print( yati_value )
+    
+
+        score= {
+                    'n_paadalu': weightage_factor* paadam_count/ config["n_paadalu"],
+                    'gana_kramam': weightage_factor* gana_kramam_score/ (config["n_paadalu"]*len(config["gana_kramam"])),
+                    'yati_sthanam': weightage_factor* sum(match_yati)/ config["n_paadalu"],
+                }
+        
+        overall_score= sum(score.values())/ len(score)
+
+        if overall_score == 1:
+            print("Padyam Detected: ", type.upper())
+        
+        else:
+            print("Padyam not exactly matched with: ", type.upper())
+
+        return {"chandassu_score": overall_score, "micro_score": score}
+        
+    except Exception as e:
+        print( "Exception Occurred: ", str(e) )
+
+
+def check_vruttam( 
+                    lg_data, type, verbose= True, weightage_normalization= True,
+                    weightage_factor= {"n_paadalu": 1, "n_aksharalu": 1, "gana_kramam": 1, "yati_sthanam": 1, "prasa": 1}                  
+                ):
+
+    try:
+        if weightage_normalization:
+
+            total= sum(weightage_factor.values())
+
+            weightage_factor= {i:j/total for i, j in weightage_factor.items()}
+            
+
         config= getattr(Vruttamu, type, False)
 
         if verbose:
@@ -68,11 +168,11 @@ def check_vruttam( lg_data, type, weightage_factor= 1, verbose= True ):
                                 )
 
         # 'weightage_factor' can be modified for more insights
-        score["n_paadalu"]= weightage_factor*count_paadam/ config["n_paadalu"]
-        score["n_aksharalu"]= weightage_factor* count_aksharam/ (config["n_paadalu"]*config["n_aksharalu"])
-        score["gana_kramam"]= weightage_factor* gana_kramam_score/ (config["n_paadalu"]*len(config["gana_kramam"]))
-        score["yati_sthanam"]= weightage_factor*sum(match_yati)/ config["n_paadalu"]
-        score["prasa"]= weightage_factor*max( match_prasa.values() )/ config["n_paadalu"]
+        score["n_paadalu"]= weightage_factor["n_paadalu"]*count_paadam/ config["n_paadalu"]
+        score["n_aksharalu"]= weightage_factor["n_aksharalu"]* count_aksharam/ (config["n_paadalu"]*config["n_aksharalu"])
+        score["gana_kramam"]= weightage_factor["gana_kramam"]* gana_kramam_score/ (config["n_paadalu"]*len(config["gana_kramam"]))
+        score["yati_sthanam"]= weightage_factor["yati_sthanam"]*sum(match_yati)/ config["n_paadalu"]
+        score["prasa"]= weightage_factor["prasa"]*max( match_prasa.values() )/ config["n_paadalu"]
 
         for sub_score in ["n_paadalu", "n_aksharalu", "gana_kramam", "yati_sthanam", "prasa"]:
 
